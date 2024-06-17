b3872f0353a286ed2e8c8055218f44e6209b3403
Kushal V <kushal@sahu.cs.unm.edu>
2024-06-17 06:24:19 -0600

    moving solar elastosis files to respective folders

--- code/clam/solar_elastosis_data_processor/process_csv.py
+++ code/clam/solar_elastosis_data_processor/process_csv.py
@@ -1,8 +1,9 @@
 import pandas as pd
+import random
+ 
+eval= True
 
-
-
-file_name='/home/kushal/data/gem_blgm_results_uni_conch_224_path/process_list_autogen.csv'
+file_name='/home/kushal/data/tcgamel_clam/segmentation/process_list_autogen.csv'
 
 df = pd.read_csv(file_name)
 
@@ -13,6 +14,21 @@ df =df.iloc[:-1]
 
 df['slide_id']=df['slide_id'].str.replace('.svs','',case=False)
 
+if eval== False:
+    print(df.head())
+    df.to_csv('process_list_autogen.csv',index=False)
 
-print(df.head())
-df.to_csv('process_list_autogen_refined_uni_224.csv',index=False)
+if eval == True:
+    dest_file ="dataset_csv/tumor_vs_normal_dummy_clean.csv"
+    fake_label_df=df.filter(['slide_id'],axis=1)
+    print(fake_label_df.head())
+    num_array=[random.randint(1,2) for i in range(len(df))]
+    print(num_array)
+    print(len(num_array))
+    label_dict = {1:"tumor_tissue",2:"normal_tissue"}
+    labels = [label_dict[i] for i in num_array]
+    fake_label_df =fake_label_df.assign(label=labels)
+    case_id =[i for i in range(len(df))]
+    fake_label_df =fake_label_df.assign(case_id=case_id)
+    print(fake_label_df.head())
+    fake_label_df.to_csv(dest_file,index=False)
